e08f39867e6c44c646cbae8fe8bab232d23b040a
FaustaAkbar <faussss1357@gmail.com>
2024-07-19 23:40:41 +0700

    Test listView

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(myApp());
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -12,28 +12,20 @@ class myApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
+          title: Text("List View sparated"),
           centerTitle: true,
-          title: Text(
-            "My App",
-          ),
           backgroundColor: Colors.blue,
         ),
-        body: Center(
-          child: Text(
-            " Budi bangun lebih awal dari ayam-ayamnya dan bekerja keras di ladang kecilnya, menanam berbagai macam sayuran. Meski hidup sederhana, Budi tak pernah mengeluh. Ia selalu tersenyum, menyapa semua orang dengan ramah, dan membantu tetangganya tanpa pamrih. Suatu hari, seorang pelancong dari kota besar tersesat dan singgah di desa itu",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                decoration: TextDecoration.underline,
-                decorationStyle: TextDecorationStyle.wavy,
-                decorationThickness: 3,
-                decorationColor: Colors.green,
-                backgroundColor: Colors.yellow,
-                fontFamily: 'Fausta',
-                fontWeight: FontWeight.bold,
-                letterSpacing: 3,
-                color: Colors.red,
-                fontSize: 20),
-          ),
+        body: ListView.separated(
+          itemCount: 4,
+          separatorBuilder: (context, index) {
+            return Divider(
+              color: Colors.black,
+            );
+          },
+          itemBuilder: (context, index) {
+            return Text("hallo");
+          },
         ),
       ),
     );
