0a8352282dc575e6507fb78168a8d1de82b970e9
FaustaAkbar <faussss1357@gmail.com>
2024-07-28 11:45:58 +0700

    AppBarNew

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final List<Tab> myTab = [
+  final List<Tab> myList = [
     Tab(
       text: "Tab 1",
-      icon: Icon(Icons.favorite),
     ),
     Tab(
-      icon: Icon(Icons.stacked_bar_chart),
-    ),
-    Tab(
-      text: "Tab 3",
-    ),
-    Tab(
-      child: Text(
-        "Tab 4",
-      ),
-    ),
+      text: "Tab 2",
+    )
   ];
   @override
   Widget build(BuildContext context) {
@@ -28,62 +19,85 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: DefaultTabController(
-        initialIndex: 3, //Untuk Tab ke berapa yang mau terbuka dahulu
-        length: myTab.length,
-        child: Scaffold(
-          appBar: AppBar(
-              centerTitle: true,
+          initialIndex: 1,
+          length: myList.length,
+          child: Scaffold(
+            appBar: AppBar(
               backgroundColor: Colors.blue,
-              title: Text("Haiiii"),
               bottom: PreferredSize(
-                  // untuk Membuat bootom
-                  preferredSize: Size.fromHeight(100),
-                  child: /* DefaultTabController( // Bisa menggunakan ini tanpa membungkus Scaffold
-                  length: 3,
-                  child: TabBar(tabs: [
-                    Tab(
-                      text: "Tab 1",
-                      icon: Icon(Icons.favorite),
-                    ),
-                    Tab(
-                      text: "Tab 2",
-                    ),
-                    Tab(
-                      text: "Tab 3",
-                    ),
-                  ]),
-                ), */
-                      TabBar(
-                    unselectedLabelColor:
-                        Colors.white, //warna saat tidak di pilih
-                    labelColor: Colors.black, //warna ketika dipilih
-                    unselectedLabelStyle: TextStyle(
-                        fontFamily:
-                            "Schyler"), //style font ketika tidak dipilih
-                    tabs: myTab,
-                    indicatorSize: TabBarIndicatorSize
-                        .tab, //supaya bisa full tab(bukan mengikuti ukuran chid)
-                    // indicatorColor: Colors.yellow,
-                    // indicatorWeight: 5,
-                    // indicatorPadding: EdgeInsets.all(10),
+                  preferredSize: Size.fromHeight(200),
+                  child: TabBar(
+                    indicatorSize: TabBarIndicatorSize.tab,
+                    tabs: myList,
+                    unselectedLabelColor: Colors.yellow,
+                    splashBorderRadius: BorderRadius.circular(20),
+                    indicatorColor: const Color.fromARGB(255, 0, 0, 0),
                     indicator: BoxDecoration(
-                        border: Border.all(
-                            color: Colors.blue, width: 1 //Untuk lebar border
-                            ),
-                        color: Colors.white, //warna tab dalam
-                        shape: BoxShape.rectangle, //Bentuk tab(Kotak,Bulat)
-                        borderRadius:
-                            BorderRadius.circular(10) //Untuk ukuran Lengkungan
-                        ),
-                  ))),
-          body: TabBarView(children: [
-            Center(child: Text("Content for Tab 1")),
-            Center(child: Text("Content for Tab 2")),
-            Center(child: Text("Content for Tab 3")),
-            Center(child: Text("Content for Tab 4")),
-          ]),
-        ),
-      ),
+                        color: Colors.yellow,
+                        border: Border.all(color: Colors.black, width: 5),
+                        /* Border(
+                            bottom: BorderSide(color: Colors.black, width: 5)), */
+                        borderRadius: BorderRadius.circular(20)),
+                  )),
+            ),
+            body: TabBarView(children: [
+              Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: Colors.red,
+                  ),
+                  height: 200,
+                  width: 300,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          IconButton(
+                              onPressed: () {}, icon: Icon(Icons.favorite)),
+                          Container(
+                            width: 10,
+                            height: 10,
+                            color: Colors.blue,
+                          ),
+                          Container(
+                            width: 10,
+                            height: 10,
+                            color: Colors.yellow,
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            width: 10,
+                            height: 10,
+                            color: Colors.black,
+                          ),
+                          Container(
+                            width: 10,
+                            height: 10,
+                            color: Colors.blue,
+                          ),
+                          Container(
+                            width: 10,
+                            height: 10,
+                            color: Colors.yellow,
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Center(
+                child: Text("This is tab Number two"),
+              ),
+            ]),
+          )),
     );
   }
 }
